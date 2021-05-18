e8dffff1a66e8681c4083d2e4a432686f01e16f0
Eric Greene <eric@t4d.io>
2021-05-18 17:06:44 -0400

    add code demos

--- react-app/src/components/ColorTool.js
+++ react-app/src/components/ColorTool.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 export const ColorTool = (props) => {
+  const [
+    colorForm, // current state date
+    setColorForm, // updates the state, and trigger a re-render
+  ] = useState(
+    {
+      name: "",
+      hexcode: "",
+    } /* initial state value */
+  );
+
+  const change = (e) => {
+    setColorForm({
+      ...colorForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const headerText = "Color Tool";
+
+  console.log(colorForm);
 
   return (
     <>
@@ -11,6 +32,28 @@ export const ColorTool = (props) => {
           <li key={color.id}>{color.name}</li>
         ))}
       </ul>
+      <form>
+        <div>
+          <label htmlFor="color-name-input">Color Name:</label>
+          <input
+            id="color-name-input"
+            type="text"
+            name="name"
+            value={colorForm.name}
+            onChange={change}
+          />
+        </div>
+        <div>
+          <label htmlFor="color-hexcode-input">Color Hexcode:</label>
+          <input
+            id="color-hexcode-input"
+            type="text"
+            name="hexcode"
+            value={colorForm.hexcode}
+            onChange={change}
+          />
+        </div>
+      </form>
     </>
   );
 };
