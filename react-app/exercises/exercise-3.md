677aa1b69279fbf496a417a985e923f545966b6c
Eric Greene <eric@t4d.io>
2021-05-18 14:03:07 -0400

    add code demos

---
+++ react-app/exercises/exercise-3.md
@@ -0,0 +1,5 @@
+# Exercise 3
+
+1. Using the list of colors, display the list of colors dynamically in the color tool.
+
+2. Ensure it works!
--- react-app/src/components/ColorTool.js
+++ react-app/src/components/ColorTool.js
@@ -1,18 +1,21 @@
 export const ColorTool = () => {
+  const headerText = "Color Tool";
+  const colors = ["green", "white", "saffron", "red", "gold", "blue", "black"];
+
   return (
     <>
       <header>
-        <h1>Color Tool</h1>
+        <h1>{headerText}</h1>
       </header>
       <ul>
-          <li>green</li>
-          <li>white</li>
-          <li>saffron</li>
-          <li>red</li>
-          <li>gold</li>
-          <li>white</li>
-          <li>blue</li>
-          <li>black</li>
+        <li>green</li>
+        <li>white</li>
+        <li>saffron</li>
+        <li>red</li>
+        <li>gold</li>
+        <li>white</li>
+        <li>blue</li>
+        <li>black</li>
       </ul>
     </>
   );
