d17b311c2b5fb3f9eb336f44c646ff4c8a28db89
Eric Greene <eric@t4d.io>
2021-05-20 14:58:27 -0400

    add tool header component

--- angular-app/src/app/color-tool/color-tool.module.ts
+++ angular-app/src/app/color-tool/color-tool.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { ColorHomeComponent } from './components/color-home/color-home.component';
 
 @NgModule({
   declarations: [ColorHomeComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, SharedModule],
   exports: [ColorHomeComponent],
 })
 export class ColorToolModule {}
---
+++ angular-app/src/app/shared/components/tool-header/tool-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-header',
+  templateUrl: './tool-header.component.html',
+  styleUrls: ['./tool-header.component.css'],
+})
+export class ToolHeaderComponent implements OnInit {
+  @Input()
+  headerText = 'The Tool';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
