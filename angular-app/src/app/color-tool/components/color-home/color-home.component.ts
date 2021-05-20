2b5ce9552e4096953ae8458bd37e7cfb3b9b7700
Eric Greene <eric@t4d.io>
2021-05-20 12:56:05 -0400

    add color tool code demos

--- angular-app/src/app/color-tool/components/color-home/color-home.component.ts
+++ angular-app/src/app/color-tool/components/color-home/color-home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Color } from '../../models/color';
+
 @Component({
   selector: 'app-color-home',
   templateUrl: './color-home.component.html',
-  styleUrls: ['./color-home.component.css']
+  styleUrls: ['./color-home.component.css'],
 })
 export class ColorHomeComponent implements OnInit {
+  headerText = 'Color Tool';
 
-  constructor() { }
+  colors: Color[] = [
+    { id: 1, name: 'red', hexcode: 'ff0000' },
+    { id: 2, name: 'green', hexcode: '00ff00' },
+    { id: 3, name: 'blue', hexcode: '0000ff' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
