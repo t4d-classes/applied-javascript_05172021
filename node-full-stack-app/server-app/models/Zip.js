0c2fdb001677945521d03eabec87aab9bc0409c3
Eric Greene <eric@t4d.io>
2021-05-21 16:28:43 -0400

    add models

---
+++ node-full-stack-app/server-app/models/Zip.js
@@ -0,0 +1,13 @@
+
+module.exports.Zip = global.sampleTrainingConn.model(
+  'Zip',
+  {
+    city: String,
+    zip: String,
+    pop: Number,
+    state: String,
+    loc: {
+      x: Number,
+      y: Number,
+    },
+  });
